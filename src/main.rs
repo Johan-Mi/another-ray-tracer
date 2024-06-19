ff813e1b24fbaf365800862d743504828eeb6cd0
Johan-Mi <johanmi@protonmail.com>
2024-06-19 14:53:53 +0200

    Load scene from file

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use image::Image;
 mod ray;
 use ray::{Hit, Ray};
 mod renderer;
+mod scene;
 mod triangle;
 use triangle::Triangle;
 
@@ -26,20 +27,14 @@ type WorldPoint = euclid::Point3D<f32, WorldSpace>;
 type WorldVector = euclid::Vector3D<f32, WorldSpace>;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let camera = Camera {
-        transform: euclid::RigidTransform3D {
-            rotation: euclid::Rotation3D::identity(),
-            translation: euclid::Vector3D::zero(),
-        },
-        // https://en.wikipedia.org/wiki/Field_of_view
-        vertical_field_of_view: euclid::Angle::degrees(150.0),
-    };
+    let scene =
+        ron::from_str::<scene::Scene>(&std::fs::read_to_string("scene.ron")?)?;
 
     renderer::render(
-        &mesh::load(Path::new("mesh.obj"))?,
-        &camera,
+        &mesh::load(Path::new(&scene.mesh))?,
+        &scene.camera,
         ScreenSize::new(480, 360),
-        &Image::open(Path::new("skybox.png"))?,
+        &Image::open(Path::new(&scene.skybox))?,
         Path::new("image.ppm"),
     )?;
 
---
+++ src/scene.rs
@@ -0,0 +1,57 @@
+use crate::{Camera, WorldPoint};
+use euclid::{RigidTransform3D, Rotation3D, Vector2D};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Scene {
+    pub camera: Camera,
+    pub skybox: String,
+    pub mesh: String,
+}
+
+impl<'de> Deserialize<'de> for Camera {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        struct Camera {
+            position: WorldPoint,
+            look_at: WorldPoint,
+            vertical_field_of_view: Angle,
+        }
+
+        Camera::deserialize(deserializer).map(|it| {
+            let direction = (it.look_at - it.position).normalize();
+            let pitch = euclid::Angle::radians(direction.y.asin());
+            let forward = Vector2D::new(0.0, 1.0);
+            let yaw = direction.xz().angle_to(forward);
+            Self {
+                transform: RigidTransform3D {
+                    rotation: Rotation3D::euler(
+                        euclid::Angle::default(),
+                        pitch,
+                        yaw,
+                    ),
+                    translation: it.position.to_vector(),
+                },
+                vertical_field_of_view: it.vertical_field_of_view.into(),
+            }
+        })
+    }
+}
+
+#[derive(Deserialize)]
+enum Angle {
+    Radians(f32),
+    Degrees(f32),
+}
+
+impl From<Angle> for euclid::Angle<f32> {
+    fn from(value: Angle) -> Self {
+        match value {
+            Angle::Radians(n) => Self::radians(n),
+            Angle::Degrees(n) => Self::degrees(n),
+        }
+    }
+}
